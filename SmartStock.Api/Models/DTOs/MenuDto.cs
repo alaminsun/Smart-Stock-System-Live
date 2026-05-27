b9e1592c7387de5d81c3588c21933a0fcfa07762
Md Al Amin <sunmoon843@gmail.com>
2026-05-27 02:35:47 +0200

    commit 8

---
+++ SmartStock.Api/Models/DTOs/MenuDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartStock.Api.Models.DTOs
+{
+    public class MenuDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Icon { get; set; }
+        public string? Link { get; set; }
+        public string? Permission { get; set; }
+        public int? ParentId { get; set; } // ফ্রন্টএন্ডের parentId এখানে ম্যাপ হবে
+        public int DisplayOrder { get; set; }
+    }
+}
